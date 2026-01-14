21dfd1a72e56fdb7cf33b42e66127243a13a0b2a
haaem <serrysallylee@gmail.com>
2026-01-14 17:05:10 +0900

    🎉 Backend project start

---
+++ echoforest-backend/src/test/java/com/d105/EchoforestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoforestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
