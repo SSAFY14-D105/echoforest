680e77df8f00dba3ed713454f0438cd374edb4e6
이태희 <k58701@naver.com>
2026-01-22 10:30:49 +0900

    🔥 [BE] hotfix: 요청데이터 통일

--- echoforest-backend/src/main/java/com/d105/dto/TokenReqDto.java
+++ echoforest-backend/src/main/java/com/d105/dto/TokenReqDto.java
@@ -1,6 +1,5 @@
 package com.d105.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty; // 추가 필요
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,9 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TokenReqDto {
 
-    // 프론트엔드가 "roomName"으로 보내더라도 자바의 "roomId"에 매핑되도록 설정
     @Schema(description = "입장할 게임 방 번호", example = "room_1")
-    @JsonProperty("roomName")
     private String roomId;
 
     @Schema(description = "유저 고유 ID", example = "user_1234")
