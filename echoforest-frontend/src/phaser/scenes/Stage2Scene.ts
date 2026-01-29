c4d33c86d7d9c7824021f6083ba1dd0284f6ba5b
haaem <serrysallylee@gmail.com>
2026-01-29 17:08:57 +0900

    ✏️ [FE] fix: 랜더링 비동기로 여러 프레임에 나누어 처리하도록 수정

--- echoforest-frontend/src/phaser/scenes/Stage2Scene.ts
+++ echoforest-frontend/src/phaser/scenes/Stage2Scene.ts
@@ -54,10 +54,12 @@ export default class Stage2Scene extends BaseGameScene {
         this.mapManager = new MapManager(this, 'stage_02_map');
         this.offsetY = this.mapManager.getOffsetY();
 
-        // 5. 맵 생성 (Tileset Name, Phaser Cache Key, Background Key)
-        this.mapManager.initialize('tiles_tileset', 'tiles_tileset', 'background_image');
-
-        super.create();
+        // 비동기 맵 초기화 (충돌체 생성 시 프레임 드롭 방지)
+        this.mapManager.initializeAsync('tiles_tileset', 'tiles_tileset', 'background_image')
+            .then(() => {
+                console.log('[Stage2Scene] Async map initialization complete');
+                super.create();
+            });
     }
 
     protected createGimmicks(): void {
